cafc808f2821607927606af7d875df0c22ee2412
Liam Spatola <liam.spatola@gmail.com>
2025-08-02 21:27:08 +1000

    Cleaned up the formatting

--- models/count.py
+++ models/count.py
@@ -5,7 +5,13 @@ from models.currency import Currency
 
 
 class Count(Database):
-    def __init__(self, user: User, currency: Currency, date: datetime = datetime.now(), count_id: int | None = None):
+    def __init__(
+        self,
+        user: User,
+        currency: Currency,
+        date: datetime = datetime.now(),
+        count_id: int | None = None,
+    ):
         self.user: User = user
         self.currency: Currency = currency
         self.date: datetime = date
@@ -14,14 +20,16 @@ class Count(Database):
     # ORM Methods
     def exists(self) -> bool:
         if self.count_id:
-            sql: str = "SELECT EXISTS(SELECT 1 FROM counts WHERE count_id = ?) AS count_exists"
+            sql: str = (
+                "SELECT EXISTS(SELECT 1 FROM counts WHERE count_id = ?) AS count_exists"
+            )
             params: tuple = (self.count_id,)
             result = self.query(sql, params)
 
             row = result[0]
             return bool(row["count_exists"])
         return False
-    
+
     def save(self) -> int | None:
         if self.exists():
             self._update()
@@ -29,17 +37,28 @@ class Count(Database):
             new_count_id = self._insert()
             self.count_id = new_count_id
             return new_count_id
-        
+
     def _update(self) -> None:
-        sql: str = "UPDATE counts SET user = ?, date = ?, currency = ? WHERE count_id = ?"
-        params: tuple = (self.user.user_id, self.date.isoformat(" "), self.currency.currency_id, self.count_id)
+        sql: str = (
+            "UPDATE counts SET user = ?, date = ?, currency = ? WHERE count_id = ?"
+        )
+        params: tuple = (
+            self.user.user_id,
+            self.date.isoformat(" "),
+            self.currency.currency_id,
+            self.count_id,
+        )
         self.query(sql, params)
-    
+
     def _insert(self) -> int:
         sql: str = "INSERT INTO counts (user, date, currency) VALUES (?, ?, ?)"
-        params: tuple = (self.user.user_id, self.date.isoformat(" "), self.currency.currency_id)
+        params: tuple = (
+            self.user.user_id,
+            self.date.isoformat(" "),
+            self.currency.currency_id,
+        )
         return self.query(sql, params)
-    
+
     # Utility Methods
     @classmethod
     def load_by_count_id(cls, count_id: int) -> "Count" | None:
@@ -51,9 +70,14 @@ class Count(Database):
         if result:
             # Using the results to build a count object
             row = result[0]
-            return cls(User.load_by_user_id(row["user"]), Currency.load_by_currency_id(row["currency_id"]), datetime.strptime(row["date"], "%Y-%m-%d %H:%M:%S"), count_id = row["count_id"])
+            return cls(
+                User.load_by_user_id(row["user"]),
+                Currency.load_by_currency_id(row["currency_id"]),
+                datetime.strptime(row["date"], "%Y-%m-%d %H:%M:%S"),
+                count_id=row["count_id"],
+            )
         return None
-    
+
     @classmethod
     def get_all_counts(cls) -> list["Count" | None]:
         db = Database()
@@ -62,10 +86,17 @@ class Count(Database):
 
         counts = list()
         for row in result:
-            counts.append(cls(User.load_by_user_id(row["user"]), Currency.load_by_currency_id(row["currency_id"]), datetime.strptime(row["date"], "%Y-%m-%d %H:%M:%S"), count_id = row["count_id"]))
-        
+            counts.append(
+                cls(
+                    User.load_by_user_id(row["user"]),
+                    Currency.load_by_currency_id(row["currency_id"]),
+                    datetime.strptime(row["date"], "%Y-%m-%d %H:%M:%S"),
+                    count_id=row["count_id"],
+                )
+            )
+
         return counts
-    
+
     def calculate_total_value(self) -> float:
         # TODO: Implement this code
         pass
--- models/denomination.py
+++ models/denomination.py
@@ -3,7 +3,9 @@ from models.currency import Currency
 
 
 class Denomination(Database):
-    def __init__(self, currency: Currency, value: float, denomination_id: int | None = None) -> None:
+    def __init__(
+        self, currency: Currency, value: float, denomination_id: int | None = None
+    ) -> None:
         self.currency: Currency = currency
         self.value: float = value
         self.denomination_id: int | None = denomination_id
@@ -11,14 +13,16 @@ class Denomination(Database):
     # ORM Methods
     def exists(self) -> bool:
         if self.denomination_id:
-            sql: str = "SELECT EXISTS(SELECT 1 FROM denominations WHERE denomination_id = ?) AS denomination_exists"
+            sql: str = (
+                "SELECT EXISTS(SELECT 1 FROM denominations WHERE denomination_id = ?) AS denomination_exists"
+            )
             params: tuple = (self.denomination_id,)
             result = self.query(sql, params)
 
             row = result[0]
             return bool(row["denomination_exists"])
         return False
-    
+
     def save(self) -> int | None:
         if self.exists():
             self._update()
@@ -26,17 +30,19 @@ class Denomination(Database):
             new_denomination_id = self._insert()
             self.count_id = new_denomination_id
             return new_denomination_id
-        
+
     def _update(self) -> None:
-        sql: str = "UPDATE denominations SET currency = ?, value = ? WHERE denomination_id = ?"
+        sql: str = (
+            "UPDATE denominations SET currency = ?, value = ? WHERE denomination_id = ?"
+        )
         params: tuple = (self.currency.currency_id, self.value, self.denomination_id)
         self.query(sql, params)
-    
+
     def _insert(self) -> int:
         sql: str = "INSERT INTO denominations (currency, value) VALUES (?, ?)"
         params: tuple = (self.currency.currency_id, self.value)
         return self.query(sql, params)
-    
+
     # Utility Methods
     @classmethod
     def load_by_denomination_id(cls, denomination_id: int) -> "Denomination" | None:
@@ -48,9 +54,13 @@ class Denomination(Database):
         if result:
             # Using the results to build a denomination object
             row = result[0]
-            return cls(Currency.load_by_currency_id(result["currency"]), row["value"], denomination_id = row["denomination_id"])
+            return cls(
+                Currency.load_by_currency_id(result["currency"]),
+                row["value"],
+                denomination_id=row["denomination_id"],
+            )
         return None
-    
+
     @classmethod
     def get_all_denominations(cls) -> list["Denomination" | None]:
         db = Database()
@@ -59,6 +69,12 @@ class Denomination(Database):
 
         denominations = list()
         for row in result:
-            denominations.append(cls(Currency.load_by_currency_id(result["currency"]), row["value"], denomination_id = row["denomination_id"]))
-        
+            denominations.append(
+                cls(
+                    Currency.load_by_currency_id(result["currency"]),
+                    row["value"],
+                    denomination_id=row["denomination_id"],
+                )
+            )
+
         return denominations
