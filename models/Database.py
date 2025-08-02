dd2397003ddf3fb8edde0acc8067594797ae5af3
Liam Spatola <liam.spatola@gmail.com>
2025-08-02 12:25:35 +1000

    Added a Database model

---
+++ models/Database.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+
+class Database:
+    __db_name: str
+
+    def __init__(self, db_name: str = "cashcounter.db") -> None:
+        self.__db_name = db_name
+
+    
+    def query(self, sql: str, params: tuple = tuple()) -> list[tuple] | int | None:
+        with sqlite3.connect(self.__db_name) as conn:
+            sql = sql.strip().lower() # Cleaning up the query
+            conn.row_factory = sqlite3.Row # Ensuring the column names are returned in the result
+            cur = conn.cursor
+            cur.execute(sql, params)
+
+            if sql.startswith("select"):
+                return cur.fetchall() # Returning the results for SELECT queries
+            elif sql.startswith("insert"):
+                conn.commit()
+                return cur.lastrowid # Returning the ID of the last row inserted for INSERT queries
+            else:
+                conn.commit() # For all other queries, returning None
+            
+        return None
